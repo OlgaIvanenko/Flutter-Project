255f8bf75d0d255bcf636cc0bdc0af460df26040
OlgaIvanenko <63466451+OlgaIvanenko@users.noreply.github.com>
2020-12-23 23:36:42 +0200

    Add files via upload

---
+++ lib/resourses/strings.dart
@@ -0,0 +1,15 @@
+class Strings {
+  Strings._();
+  static const fakeApiBaseUrl = "https://jsonplaceholder.typicode.com";
+  static const todoUrl = "/todos";
+
+  static const noData = "Нет данных";
+
+  static const appTitle = "Заметки";
+  static const addNote = "Добавить заметку";
+  static const errorMessage = "Ошибка";
+  static const todosTitle = "Список дел";
+
+  // HomeTodoPage
+  static const initialTodoStateLabel = "Загрузите список дел";
+}
